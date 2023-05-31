e3d77f84697c0089336c38d81e94c6c1574ec13b
rutexd <rutexgreat@gmail.com>
2023-05-31 23:54:56 +0200

    initial test

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    let i = 0;
+    setInterval(() => {
+        if (i === 5) {
+            res.end();
+        }
+        res.write(`Hello World! ${i}\n`);
+        i++;
+    }, 1000);
+});
+
+app.listen(3000, () => {  
+    console.log("Server running on port 3000");
+});
